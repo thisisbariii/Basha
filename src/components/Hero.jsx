0c68e3c1abe17dfd0db8106eff32ee791cb4d208
Shaikh Badsha <143876827+thisisbariii@users.noreply.github.com>
2025-05-05 17:00:13 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -36,14 +36,16 @@ const Hero = () => {
         >
           Contact With Me
         </button>
-        <a
-          href="/resume.pdf"
-          download="Abdul_Basha_Resume"
-          className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white
-          transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full inline-block"
-        >
-          Resume
-        </a>
+      <a
+  href="https://drive.google.com/uc?export=download&id=1iesQdYlbSVR5ZaNONh0GZWnMjyqgyBF_"
+  target="_blank"
+  rel="noopener noreferrer"
+  className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white
+  transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full inline-block"
+>
+  Download Resume
+</a>
+
       </div>
     </div>
   );
